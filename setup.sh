c9444fb96fab7fc5021fd98b2bbccbf471d969a2
Marc Richter <>
2020-10-09 12:31:31 -0400

    added chmod and chown to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 cp foo.exe /usr/local/bin/foo.exe
+chown root:root /usr/local/bin/foo.exe
+chmod 755 /usr/local/bin/foo.exe
 cp foo.service /etc/systemd/system/foo.service
+chown root:root /etc/systemd/system/foo.service
+chmod 644 /etc/systemd/system/foo.service
 
