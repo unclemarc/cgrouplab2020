8a937130aafef5bbf86aab4fa5dc50f4128be7d5
Marc Richter <>
2020-10-12 11:29:50 -0400

    install stress

--- setup.sh
+++ setup.sh
@@ -5,4 +5,8 @@ chmod 755 /usr/local/bin/foo.exe
 cp foo.service /etc/systemd/system/foo.service
 chown root:root /etc/systemd/system/foo.service
 chmod 644 /etc/systemd/system/foo.service
+# install epel and then stress
+
+yum -y install epel-release-8-8.el8.noarch.rpm
+yum -y install stress
 
