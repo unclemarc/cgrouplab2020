3b438252f450bc0c79df7796ea0cbce979a0ef7c
Marc Richter <>
2020-10-19 11:01:18 -0400

    adding users

--- setup.sh
+++ setup.sh
@@ -5,6 +5,15 @@ chmod 755 /usr/local/bin/foo.exe
 cp foo.service /etc/systemd/system/foo.service
 chown root:root /etc/systemd/system/foo.service
 chmod 644 /etc/systemd/system/foo.service
+
+# make some users
+useradd -u 6000 elsa
+useradd -u 6001 anna
+echo "redhat" | passwd --stdin elsa
+echo "redhat" | passwd --stdin anna
+
+
+
 # install epel and then stress
 
 yum -y install epel-release-8-8.el8.noarch.rpm
